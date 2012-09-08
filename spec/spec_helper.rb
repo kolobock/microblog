5ae7c8b50aadb6efb9b74a7981206c7253fd5184
Andrew Rudenko <kolobock@gmail.com>
2012-09-08 13:37:32 +0300

    added color rpec output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,4 +32,14 @@ RSpec.configure do |config|
 
   # Devise test helper
   config.include Devise::TestHelpers, :type => :controller
+  
+# Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+
 end
