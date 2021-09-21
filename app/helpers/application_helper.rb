6eeb404f4f9df1932ddbe8003eb07f89ed82d986
Andrew Rudenko <kolobock@gmail.com>
2021-09-21 09:44:14 +0300

    some old uncommited changes. do not know why it is here. will commit anyway to keep old minds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
   ## Output flash messages
   def render_flash_messages
     flash.collect do |key, msg|
-      content_tag :span, :id => 'flasher', :class=> "flash #{key}" do
-        content_tag(:span, :class => "#{key} icon") do
+      content_tag :span, id: 'flasher', class: "flash #{key}" do
+        content_tag(:span, class: "#{key} icon") do
           msg.html_safe
         end +
-            content_tag(:a, :class=>"close_flash icon") do
+            content_tag(:a, class: "close_flash icon") do
             end
       end
     end.join.html_safe unless flash.blank?
